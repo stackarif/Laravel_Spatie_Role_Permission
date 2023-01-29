e2721693eb69537a278f520bb9d6cb993c1467b6
stackarif <arifmahmudaj6@gmail.com>
2023-01-29 16:01:24 +0600

    role and permission created

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Actions\Role\CreateRole;
+use App\Actions\Role\UpdateRole;
+use Spatie\Permission\Models\Role;
+use App\Http\Controllers\Controller;
+use Brian2694\Toastr\Facades\Toastr;
+use App\Http\Requests\RoleFormRequest;
+use Spatie\Permission\Models\Permission;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roles = Role::with('permissions')->latest()->get();
+        return view('role.index', compact('roles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $permissions = Permission::all();
+        // $permission_groups = User::getPermissionGroup();
+
+        return view('role.create', compact('permissions'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' =>'required|unique:roles,name',
+
+        ]);
+        $role = Role::create(['name' => $request->name, 'guard_name' => 'web']);
+        $role->syncPermissions($request->permissions);
+
+        session()->flash('success', 'Role Created!');
+        return redirect()->route('roles.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+
+        return view('role.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update($request, $id)
+    {
+        abort_if(!userCan('role.update'), 403);
+
+        try {
+            UpdateRole::update($request, $role);
+
+            session()->flash('success', 'Role Updated!');
+            return back();
+        } catch (\Throwable $th) {
+
+            session()->flash('Error', 'Something is wrong');
+            return back();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Role $role)
+    {
+        // abort_if(!userCan('role.delete'), 403);
+
+        try {
+            if (!is_null($role)) {
+                $role->delete();
+            }
+            session()->flash('success', 'Role Deleted!');
+            return back();
+        } catch (\Throwable $th) {
+            session()->flash('Error', 'Something is wrong');
+            return back();
+        }
+    }
+}
---
+++ resources/views/role/index.blade.php
@@ -0,0 +1,126 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            User Roles
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+                <div class="flex justify-between items-center p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                    <h2 class="">All Roles
+                        <span class="bg-blue-500 text-white rounded px-1 text-xs py-0.5">10</span>
+                    </h2>
+                    <a href="{{ route('roles.create') }}">
+                        <button type="button"
+                            class="text-white bg-blue-500 hover:bg-blue-400 font-bold focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                            Create Role
+                        </button>
+                    </a>
+                </div>
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="py-3 px-6">
+                                SL.
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Name
+                            </th>
+                            <th scope="col" class="py-3 px-6" style="width: 500px">
+                                Permissions
+                            </th>
+                            <th scope="col" class="py-3 px-6">
+                                Actions
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if ($roles->count())
+                            @foreach ($roles as $role)
+                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row" class="py-4 px-6">
+                                        {{$role->id}}
+
+                                    </th>
+
+                                    <th scope="row" class="py-4 px-6">
+                                        {{$role->name}}
+                                    </th>
+
+                                    <th class="py-4 px-6" style="width: 500px">
+                                        <button id="showPerIcon{{$role->id}}" onclick="permissionShow('show', {{$role->id}})" type="button"
+                                            data-tooltip-target="show-button" data-bs-toggle="tooltip" data-bs-placement="top">
+                                            <x-svg.eye class="w-6 h-6 text-pink-400" />
+                                        </button>
+
+                                        <button class="hidden" id="hidePerIcon{{$role->id}}" onclick="permissionShow('hide', {{$role->id}})"
+                                            type="button" data-tooltip-target="hide-button" data-bs-toggle="tooltip"
+                                            data-bs-placement="top">
+                                            <x-svg.eye-off class="w-6 h-6 text-pink-400" />
+                                        </button>
+                                        <div id="permission{{$role->id}}" class="hidden flex gap-2 flex-wrap text-center">
+                                            @if ($role->permissions->count())
+                                            @foreach ($role->permissions as $item)
+                                            <div class="bg-green-500 text-white px-2 py-1 rounded font-bold">
+                                                {{$item->name}}
+                                            </div>
+                                            @endforeach
+                                            @else
+                                            <div class="bg-green-500 text-white p-1 rounded font-bold">
+                                                [ ]
+                                            </div>
+                                                
+                                            @endif
+                                         
+                                        </div>
+                                      
+                                    </th>
+                                    <td class="py-4 px-6 flex gap-2">
+                                        <a data-tooltip-target="edit-button" data-bs-toggle="tooltip" data-bs-placement="top"
+                                            href="{{route('roles.edit', $role->id)}}">
+                                            <x-svg.edit class="w-6 h-6 text-green-400" />
+                                        </a>
+
+                                        <form action="{{ route('roles.destroy', $role->id) }}" method="POST" class="d-inline">
+                                            @method('DELETE ')
+                                            @csrf
+                                            <button data-tooltip-target="delete-button" data-bs-toggle="tooltip"
+                                                data-bs-placement="top">
+                                                <x-svg.trash class="w-6 h-6 text-red-400" />
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                
+                            @endforeach
+
+                        @else
+                            <tr>
+                                <td colspan="10" class="text-center pt-8">Nothing Found.</td>
+                            </tr>
+                            
+                        @endif
+                    </tbody>
+                </table>
+                <div class="p-5">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+<script>
+    function permissionShow(param, id) {
+        if (param === 'show') {
+            $('#permission' + id).removeClass('hidden');
+            $('#showPerIcon' + id).addClass('hidden');
+            $('#hidePerIcon' + id).removeClass('hidden');
+        } else {
+            $('#permission' + id).addClass('hidden');
+            $('#showPerIcon' + id).removeClass('hidden');
+            $('#hidePerIcon' + id).addClass('hidden');
+        }
+    }
+</script>
